205bfbb4f875fc4a27c83529ee24d8a8a8021ef3
James <jameslokejw@gmail.com>
2026-06-26 15:55:50 +0100

    stuff

--- bsync/task.py
+++ bsync/task.py
@@ -6,20 +6,32 @@ class Task:
   def __init__(self, coro, loop):
     self.loop = loop
     self.coro = coro
+    self.callbacks = []
+    self._done = False
+    self._result = None
   
   def step(self, value = None):
-    future = self.coro.send(value)
-    if isinstance(future, Future):
-      future.add_done_callback(self._wakeup)
-  
+    try:
+      res = self.coro.send(value)
+      if isinstance(res, Future):
+        res.add_done_callback(self._wakeup)
+    except StopIteration as e:
+      self._done = True
+      self._result = e.value
+      for cb in callbacks:
+        self.callbacks.append(cb)
+
   def done(self) -> bool:
-    pass
+    return self._done
   
   def result(self) -> Any:
-    pass
+    return self._result
   
-  def add_done_callback(self):
-    pass
+  def add_done_callback(self, cb):
+    if task.done():
+      cb(self)
+    else:
+      self.callbacks.append(cb)
   
   def _wakeup(self, future):
     self.loop.call_soon(self.step, future.get_result())
